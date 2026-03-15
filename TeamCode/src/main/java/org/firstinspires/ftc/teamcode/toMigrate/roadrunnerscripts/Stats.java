6bb440dc278c6f813b956bbef53d60cc4fccbf25
yoyok978 <145050056+yoyok978@users.noreply.github.com>
2026-03-15 12:49:37 +0200

    bad stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/toMigrate/roadrunnerscripts/Stats.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.toMigrate.roadrunnerscripts;
-
-import com.acmerobotics.roadrunner.Pose2d;
-
-public class Stats {
-    public static Pose2d currentPose = new Pose2d(0, 0, 0);
-}
