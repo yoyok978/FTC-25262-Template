76141998e2ae5d32dee5d7f0edc2834e23b7c9a9
yoyok978 <145050056+yoyok978@users.noreply.github.com>
2026-03-15 12:50:07 +0200

    Revert "bad stuff"
    This reverts commit 6bb440dc278c6f813b956bbef53d60cc4fccbf25.

--- TeamCode/build.gradle.kts
+++ TeamCode/build.gradle.kts
@@ -4,35 +4,37 @@ plugins {
 }
 
 repositories {
-    maven("https://mymaven.bylazar.com/releases")
     maven("https://repo.dairy.foundation/releases")
 }
 
 configure<com.android.build.gradle.AppExtension> {
     namespace = "org.firstinspires.ftc.teamcode"
+    compileSdkVersion(34)
     packagingOptions { jniLibs.useLegacyPackaging = true }
 }
 
 ftc {
-    sdk {
-        TeamCode()
+    // 1. Core SDK
+    sdk.TeamCode()
 
-    }
-
+    // 2. Sloth
     dairy {
         implementation(Sloth)
     }
 
+    // 3. Panels
     ftControl {
         implementation(fullpanels)
     }
 
+    // 4. Pedro Pathing
     pedro {
         implementation(core)
         implementation(ftc)
         implementation(telemetry)
     }
 
+    // 5. SolversLib
     solvers {
         implementation(core)
         implementation(pedroPathing)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/toMigrate/roadrunnerscripts/Stats.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.toMigrate.roadrunnerscripts;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+public class Stats {
+    public static Pose2d currentPose = new Pose2d(0, 0, 0);
+}
